011b82eedb3d7bd80e5f79c582731c924f5950f5
Scorpio_I4 <18556134754@163.com>
2023-06-16 00:45:49 +0800

    feat: 插件-视频示例开发

--- src/router/modules/plugin.ts
+++ src/router/modules/plugin.ts
@@ -86,6 +86,15 @@ export const record: RouteRecordRaw = {
           component: () => import('@/views/plugin/editor/rich/index.vue')
         }
       ]
+    },
+    {
+      path: 'video',
+      name: 'video',
+      meta: {
+        title: '视频',
+        icon: 'mdi:video-vintage'
+      },
+      component: () => import('@/views/plugin/video/index.vue')
     }
   ]
 };
---
+++ src/views/plugin/video/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Player from 'xgplayer';
+import 'xgplayer/dist/index.min.css';
+
+defineOptions({
+  name: 'video'
+});
+
+const domRef = $ref<HTMLDivElement | null>(null);
+let player = $ref<Player | null>(null);
+
+function renderPlayer() {
+  if (!domRef) return;
+  player = new Player({
+    el: domRef,
+    url: 'https://www.w3schools.com/html/movie.mp4',
+    cssFullscreen: false
+  });
+}
+
+onMounted(() => {
+  renderPlayer();
+});
+
+onUnmounted(() => {
+  player?.destroy();
+});
+</script>
+
+<template>
+  <div>
+    <ElCard>
+      <template #header>
+        <span class="text-18px">视频</span>
+      </template>
+      <SiteLink label="Github" href="https://github.com/bytedance/xgplayer"></SiteLink>
+      <div ref="domRef" class="mt-24px"></div>
+    </ElCard>
+  </div>
+</template>
