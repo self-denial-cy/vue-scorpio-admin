856535eba07803efaedd796315b9b8ec43a0cd5d
Scorpio_I4 <18556134754@163.com>
2023-06-27 00:18:23 +0800

    feat: 组件示例-人机验证 day2 over

--- src/components/RandomCodeVerify.vue
+++ src/components/RandomCodeVerify.vue
@@ -11,12 +11,99 @@ interface Props {
   bgcRange?: [number, number];
   colorRange?: [number, number];
   lineColorRange?: [number, number];
+  lineCountRange?: [number, number];
+  dotColorRange: [number, number];
+  dotCountRange?: [number, number];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  identifyCode: 'abcd',
+  width: 110,
+  height: 36,
+  fontSizeRange: () => [16, 32],
+  bgcRange: () => [180, 240],
+  colorRange: () => [50, 160],
+  lineColorRange: () => [40, 180],
+  lineCountRange: () => [2, 5],
+  dotColorRange: () => [0, 255],
+  dotCountRange: () => [20, 30]
+});
+
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+const codeCharArr = computed(() => props.identifyCode.split(''));
+
+onMounted(() => {
+  drawCanvas();
+});
+
+watch(
+  () => props.identifyCode,
+  () => {
+    drawCanvas();
+  }
+);
+
+function randomNum(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomColor(min: number, max: number) {
+  const [r, g, b] = [randomNum(min, max), randomNum(min, max), randomNum(min, max)];
+  return `rgb(${r},${g},${b})`;
+}
+
+function drawCanvas() {
+  const ctx = canvasRef.value?.getContext('2d');
+  if (!ctx) return;
+  ctx.textBaseline = 'bottom';
+  ctx.fillStyle = randomColor(...props.bgcRange);
+  ctx.fillRect(0, 0, props.width, props.height);
+  codeCharArr.value.forEach((v, i) => drawText(ctx, v, i));
+  drawLine(ctx);
+  drawDot(ctx);
+}
+
+function drawText(ctx: CanvasRenderingContext2D, txt: string, i: number) {
+  ctx.fillStyle = randomColor(...props.colorRange);
+  ctx.font = `${randomNum(...props.fontSizeRange)}px SimHei`;
+  const x = (i + 1) * (props.width / (codeCharArr.value.length + 1));
+  const y = randomNum(props.fontSizeRange[0], props.fontSizeRange[1] - 6);
+  const deg = randomNum(-24, 24);
+  ctx.translate(x, y);
+  ctx.rotate((deg * Math.PI) / 200);
+  ctx.fillText(txt, 0, 0);
+  // 复原，准备下一次绘制
+  ctx.rotate((-deg * Math.PI) / 200);
+  ctx.translate(-x, -y);
+}
+
+function drawLine(ctx: CanvasRenderingContext2D) {
+  // 绘制干扰线
+  const count = randomNum(...props.lineCountRange);
+  for (let i = 0; i < count; i++) {
+    ctx.strokeStyle = randomColor(...props.lineColorRange);
+    ctx.beginPath();
+    ctx.moveTo(randomNum(0, props.width), randomNum(0, props.height));
+    ctx.lineTo(randomNum(0, props.width), randomNum(0, props.height));
+    ctx.stroke();
+  }
+}
+
+function drawDot(ctx: CanvasRenderingContext2D) {
+  // 绘制干扰点
+  const count = randomNum(...props.dotCountRange);
+  for (let i = 0; i < count; i++) {
+    ctx.fillStyle = randomColor(...props.dotColorRange);
+    ctx.beginPath();
+    ctx.arc(randomNum(0, props.width), randomNum(0, props.height), 1, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
 </script>
 
 <template>
   <div class="v-wrap">
-    <canvas ref="canvasRef"></canvas>
+    <canvas ref="canvasRef" :width="props.width" :height="props.height"></canvas>
   </div>
 </template>
 
--- src/views/component/verify/components/RandomCodeVerifyExample/index.vue
+++ src/views/component/verify/components/RandomCodeVerifyExample/index.vue
@@ -1,5 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const charArr = [
+  '鸡',
+  '你',
+  '太',
+  '美',
+  '练',
+  '习',
+  '两',
+  '年',
+  '半',
+  '唱',
+  '跳',
+  'r',
+  'a',
+  'p',
+  '篮',
+  '球',
+  '哎',
+  '哟',
+  '你',
+  '干',
+  '嘛'
+];
+
+let code = ref('鸡你太美');
+
+function refresh() {
+  code.value = [...charArr]
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 4)
+    .join('');
+}
+</script>
 
 <template>
-  <div></div>
+  <div class="p-24px">
+    <RandomCodeVerify></RandomCodeVerify>
+  </div>
+  <div class="p-24px">
+    <RandomCodeVerify
+      class="cursor-pointer"
+      :identify-code="code"
+      :width="220"
+      :height="56"
+      :font-size-range="[32, 48]"
+      @click="refresh"
+    ></RandomCodeVerify>
+  </div>
 </template>
